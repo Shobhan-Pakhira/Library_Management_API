916377d129da11c4b0b7ea748feef6b0a422351e
Hacking Devil <shobhanpakhira@gmail.com>
2022-09-26 22:04:19 +0530

    Update student_controller.js

--- app/Controller/student_controller.js
+++ app/Controller/student_controller.js
@@ -5,7 +5,7 @@ const http_formatter = require('../Util/http_formatter');
 //Creating Book
 const createStudent = async (request,response) => {
     try {
-        const user_Stu = await student.create(request.body);
+        const User = await student.create(request.body);
             return response.status(200).json(http_formatter(user,"Student Created Sucessfully"))
     } catch (error) {
         console.log(error)
@@ -17,7 +17,7 @@ const createStudent = async (request,response) => {
 const getStudent = async (request,response) => {
     try {
         const {pageNo, perPage} = request.query;
-        const user_Stu = await student.find({});
+        const User = await student.find({});
             return response.status(200).json(http_formatter(user,"Student Fetched Sucessfully"))
     } catch (error) {
         console.log(error)
