22dcbd4118ad054e796a794924b4e14859ff98d7
Hacking Devil <shobhanpakhira@gmail.com>
2022-10-31 18:10:45 +0530

    Update bookIssue_controller.js

--- app/Controller/bookIssue_controller.js
+++ app/Controller/bookIssue_controller.js
@@ -5,8 +5,8 @@ const http_formatter = require('../Util/http_formatter');
 const createIssue = async (request, response) => {
     try{
         const Record = await IssueDate.create(request.body);
-            return response.status(200).json(http_formatter(Record,"Recorded Issuing Date"))
-    } catch (error) {
+            return response.status(200).json(http_formatter(Record ,"Recorded Issuing Date"))
+    } catch (error) { 
         console.log(error)
         return response.status(400).json(http_formatter(Record,"Failure in Recording Issue Date"))
     }
@@ -17,10 +17,11 @@ const createIssue = async (request, response) => {
 const getIssue = async (request, response) => {
     try{
         const Record = await IssueDate.find({});
-            return response.status(200).json(http_formatter(Record,"Fetching Recorded Issuing Date Successful"))
+            return response.status(200).json(http_formatter(Record ,  "Fetching Recorded Issuing Date Successful"))
     } catch (error) {
         console.log(error)
-        return response.status(400).json(http_formatter(Record,"Failure in Fetching Issue Date"))
+        // return response.status(400).json(http_formatter(Record "Failure in Fetching Issue Date"))
+        return response.status(200).json(http_formatter(Record , "Fetching Recorded Issued Date Failed"))
     }
 };
 
